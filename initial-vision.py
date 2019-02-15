258d1f14df9504272a896690b902332dd6ceef44
Hannan Saddiq <saddiqhb@gmail.com>
2019-02-15 11:35:59 +0000

    Added ability to capture screens from video

--- initial-vision.py
+++ initial-vision.py
@@ -2,18 +2,20 @@ import numpy as np
 import cv2
 
 cap = cv2.VideoCapture(0)
+i = 0
 
-while(True):
+while (True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
-    # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+    cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+
+    if cv2.waitKey(1) & 0xFF == ord('c'):  # save on pressing 'y'
+        cv2.imwrite('c{}.png'.format(i), frame)
+        i += 1
 
 # When everything done, release the capture
 cap.release()
